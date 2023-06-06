a3aee1950d2be2d6a6dc6b2f13edc24a4441bbde
yann <jeanmaireyann.pro@gmail.com>
2023-06-06 22:56:05 +0200

    zz

---
+++ src/main/java/org/acme/EmailDTO.java
@@ -0,0 +1,20 @@
+package org.acme;
+
+public class EmailDTO {
+    private String recipient;
+    private String subject;
+    private String content;
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+}
--- src/main/java/org/acme/MailResource.java
+++ src/main/java/org/acme/MailResource.java
@@ -2,27 +2,55 @@ package org.acme;
 
 import io.quarkus.mailer.Mail;
 import io.quarkus.mailer.Mailer;
-import io.smallrye.common.annotation.Blocking;
-
 import jakarta.inject.Inject;
-import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
-
-import java.util.Collections;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/mail")
 public class MailResource {
 
-    @Inject Mailer mailer;
+    @Inject
+    Mailer mailer;
 
-    @GET
-    @Blocking
-    public void sendEmail() {
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response.ResponseBuilder sendEmail(EmailDTO emailDTO) {
+        String missingProperties = checkMissingProperties(emailDTO);
+
+        if (!missingProperties.isEmpty()) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("Missing properties: " + missingProperties);
+        }
+
         mailer.send(
-                Mail.withText("yann.jeanmaire@gmail.com",
-                        "Ahoy from Quarkus",
-                        "A simple email sent from a Quarkus application."
-                )
+                Mail.withText(emailDTO.getRecipient(), emailDTO.getSubject(), emailDTO.getContent())
         );
+        return Response.accepted();
+    }
+
+    private String checkMissingProperties(EmailDTO emailDTO) {
+        StringBuilder missingProperties = new StringBuilder();
+
+        if (emailDTO.getRecipient() == null) {
+            missingProperties.append("recipient, ");
+        }
+        if (emailDTO.getSubject() == null) {
+            missingProperties.append("subject, ");
+        }
+        if (emailDTO.getContent() == null) {
+            missingProperties.append("content, ");
+        }
+
+        // Remove trailing comma and space if there are missing properties
+        if (missingProperties.length() > 0) {
+            missingProperties.setLength(missingProperties.length() - 2);
+        }
+
+        return missingProperties.toString();
     }
 }
